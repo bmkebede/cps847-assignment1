4844e5189a9218b9fccbe079525870183b04debe
bmkebede <47882351+bmkebede@users.noreply.github.com>
2019-03-02 07:24:52 -0500

    Update test.py

--- ci_play/test.py
+++ ci_play/test.py
@@ -8,7 +8,7 @@ class TestMyFunc(unittest.TestCase):
         pass
 
     def test_increment_one_1(self):
-        self.assertEqual( my_functions.increment_by_one(1), 2)
+        self.assertEqual( my_functions.increment_by_one(1), 1)
 
     def test_increment_one_2(self):
         self.assertEqual( my_functions.increment_by_one(0), 1)
